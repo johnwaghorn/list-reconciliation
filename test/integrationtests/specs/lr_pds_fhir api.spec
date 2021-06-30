f463d39475ede86e8da0ff775dc1698276019cf2
premnhs <78733960+premnhs@users.noreply.github.com>
2021-06-30 11:20:42 +0100

    PCRM-3841-Test-Coverage (#26)

---
+++ test/integrationtests/specs/lr_pds_fhir api.spec
@@ -0,0 +1,7 @@
+# PDS FHIR API Tests
+---------------------
+Tags : nightly_runs
+
+## Ensure able to access FHIR R4 PATIENT Record sucessfully
+-----------------------------------------------------------
+* trigger fhir r4 patient api for patient_id "9000000009" and assert response as expected
---
+++ test/integrationtests/step_impl/lr_pds_fhir_api.py
@@ -0,0 +1,26 @@
+import os
+import requests
+import json
+
+from getgauge.python import step, Messages
+
+URL = "https://sandbox.api.service.nhs.uk/personal-demographics/FHIR/R4/Patient"
+ROOT = os.path.dirname(os.path.abspath(__file__))
+DATA = os.path.join(ROOT, "data")
+PDSPATIENTFILE = "pds_api_response.json"
+
+
+@step("trigger fhir r4 patient api for patient_id <patient_id> and assert response as expected")
+def run_prd_fhir_get_patient_details_api(patient_id):
+    url = URL + "/" + patient_id
+    pds_url_headers = {'accept': 'application/fhir+json','NHSD-Session-URID': '555021935107','X-Request-ID': '60E0B220-8136-4CA5-AE46-1D97EF59D068','X-Correlation-ID': '11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA'}
+    
+    response = requests.get(url,headers = pds_url_headers)
+
+    response.raise_for_status()
+    expected_response_file = os.path.join(DATA, PDSPATIENTFILE)
+
+    with open(expected_response_file) as jsonfile:
+        expected_response_body = json.load(jsonfile)
+        actual_response_body = response.json()
+        assert expected_response_body == actual_response_body, "Response payload doesnt match"
