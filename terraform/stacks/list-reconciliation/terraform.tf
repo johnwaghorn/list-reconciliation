b97d61a85ffe977adefcc4611c545e8ec786bb24
Joe Stringer <joeds13@users.noreply.github.com>
2021-07-05 15:26:55 +0100

    PCRM-3781 PreProd Deployment (#35)
    Selects the correct deployment role when in the PreProd workspace
    POCing separating jobs within a Github Action

--- terraform/stacks/list-reconciliation/terraform.tf
+++ terraform/stacks/list-reconciliation/terraform.tf
@@ -13,7 +13,8 @@ provider "aws" {
   region = "eu-west-2"
 
   assume_role {
-    role_arn = "arn:aws:iam::092420156801:role/LRTerraformDeploy"
+    # Try to find a role matching the current environment, if there isn't one default to the dev role
+    role_arn = try(local.terraform_deploy_role_arn[terraform.workspace], local.terraform_deploy_role_arn["dev"])
   }
 
   default_tags {
