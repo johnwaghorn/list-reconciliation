3ee8e0ce25d53922859cded9d8a8140bdbc30710
Joe Stringer <joe.stringer2@nhs.net>
2021-07-24 15:38:58 +0100

    PCRM-0000 Role assumption in Mesh Stack
    Ensure the Mesh stack deployment is assuming the terraform role in each
    account

--- terraform/stacks/mesh/locals.tf
+++ terraform/stacks/mesh/locals.tf
@@ -3,6 +3,12 @@ locals {
   environment        = terraform.workspace
   data_clasification = local.environment == "prod" ? "5" : "1"
 
+  terraform_deploy_role_arn = {
+    dev     = "arn:aws:iam::092420156801:role/LRTerraformDeploy"
+    preprod = "arn:aws:iam::287634746327:role/LRTerraformDeploy"
+    # prod = "arn:aws:iam::000000000000:role/LRTerraformDeploy"
+  }
+
   mesh_mailboxes = {
     default = [
       {
--- terraform/stacks/mesh/terraform.tf
+++ terraform/stacks/mesh/terraform.tf
@@ -12,6 +12,11 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 
+  assume_role {
+    # Try to find a role matching the current environment, if there isn't one default to the dev role
+    role_arn = try(local.terraform_deploy_role_arn[terraform.workspace], local.terraform_deploy_role_arn["dev"])
+  }
+
   default_tags {
     tags = local.tags
   }
