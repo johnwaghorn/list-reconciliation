27587a0c19bd4771ec376a27700af9e390e2213e
abbas-khan8 <78728539+abbas-khan8@users.noreply.github.com>
2021-08-17 13:01:30 +0100

    PCRM-0000 - Fix incorrect name in log dictionary

--- packages/lambda_code/LR_24_save_records_to_s3/lr24_lambda_handler.py
+++ packages/lambda_code/LR_24_save_records_to_s3/lr24_lambda_handler.py
@@ -29,7 +29,7 @@ class SaveRecordsToS3(LambdaApplication):
             self.log_object.write_log(
                 "LR24I01",
                 log_row_dict={
-                    "records": len(records),
+                    "count": len(records),
                     "source": source,
                     "bucket": destination_bucket,
                 },
@@ -82,7 +82,7 @@ class SaveRecordsToS3(LambdaApplication):
             self.log_object.write_log(
                 "LR24I03",
                 log_row_dict={
-                    "records": len(records),
+                    "count": len(records),
                     "source": source,
                     "bucket": destination_bucket,
                 },
